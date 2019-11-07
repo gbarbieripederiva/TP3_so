78a03c94065aa2025251e3e967cfc622f61fe50e
Guido Barbieri <gbarbieripederiva@gmail.com>
2019-11-07 17:01:59 -0300

    asdad

--- quine.c
+++ quine.c
@@ -1,5 +1,3 @@
-#include<stdio.h>
-char*i="\\#include<stdio.h>",n='\n',q='"',*p=
-"%s%cchar*i=%c%c%s%c,n='%cn',q='%c',*p=%c%c%s%c,*m=%c%c%s%c%c;%s%c",*m=
-"int main(){return!printf(p,i+1,n,q,*i,i,q,*i,q,n,q,p,q,n,q,m,q,n,m,n);}"
-;int main(){return!printf(p,i+1,n,q,*i,i,q,*i,q,n,q,p,q,n,q,m,q,n,m,n);}
+#include <stdio.h>
+char*s="#include <stdio.h>%cchar*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
+int main(void){printf(s,10,34,s,34,10,10);}
